7b36ec5bc35a1f5b6418ee9c1086365580918ef5
Taylor Parrish <twpearish@gmail.com>
2018-08-21 16:33:19 -0700

    added animations and css for project cards

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react'
 
 class Project extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      transform: 'scaleY(1)',
+    }
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+
+  onMouseEnter() {
+    this.setState({transform: 'scaleY(1.3)'})
+  }
+
+  onMouseLeave() {
+    this.setState({transform: 'scaleY(1)'})
+  }
+
   render() {
-    const { name, highlight, synopsis } = this.props
+    const { name, highlight, synopsis, link, background, color } = this.props
+
     return (
-      <div className="portfolio__card-content">
-        <h3 style={{fontSize:'50px'}}>{name}</h3>
-        <div className="project-highlight">
-          <h4>{highlight}</h4>
+      <div 
+        className="portfolio-item" 
+        style={{background: `${background}`}}
+      >
+        <a 
+          className="portfolio__popup-wrapper" 
+          href={link}
+          onMouseEnter={this.onMouseEnter} 
+          onMouseLeave={this.onMouseLeave}
+        />
+
+        <div 
+          className="portfolio__card-content"
+          style={{transform: `${this.state.transform}`, color: `${color}`}}
+        >
+          <h3 style={{fontSize:'50px'}}> {name} </h3>
+          
+          <div className="project-highlight">
+            <h4>{highlight}</h4>
+          </div>
+          
+          <div className="project-synopsis">
+            <p>{synopsis}</p>
+          </div>
+
+          <div className="portfolio__social-icons">
+            <span><i class="fab fa-github"></i></span>
+            <span><i class="fas fa-globe"></i></span>
+          </div>
         </div>
-        <div className="project-synopsis">
-          <p>{synopsis}</p>
-        </div>
+
       </div>
     )
   }
--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -13,23 +13,28 @@ class Projects extends Component {
   render() {
     return (
       <div className="portfolio-item-container">
-        <div className="portfolio-item" style={{background: '#119bc9', color:'#ffffff'}}>
-          <Project 
-            name='Expensify' 
-            highlight='placeholder'
-            synopsis='Built an expense tracking app with React and Firebase'
-          />
-        </div>
-        
-        <div className="portfolio-item" style={{background: '#F89951', color:'#ffffff'}}>
-          <Project 
+        <Project 
+          name='Expensify' 
+          highlight='placeholder'
+          synopsis='Built an expense tracking app with React and Firebase'
+          link="http://www.twitter.com/twpearish"
+          background="#119bc9"
+          color="#ffffff"
+        />
+        <Project 
             name='Dev Connector' 
             highlight='placeholder'
             synopsis='Built a social network on the MERN stack'
-          />
-        </div>
-          
-        <div className="portfolio-item portfolio-item-wide portfolio-item__pizzarovers">
+            link="http://www.linkedin.com"
+            background="#F89951"
+            color="#ffffff"
+        />
+        <div 
+          className="portfolio-item portfolio-item-wide portfolio-item__pizzarovers"
+          style={{
+            background: 'linear-gradient(to bottom, #1181a7 0%,#119bc9 100%)', 
+            color:'#ffffff'
+          }}>
           <h1>hello</h1>
         </div>
 
