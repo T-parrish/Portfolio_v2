6d8c0903767bcfed75f920a533a0ff53f298c46c
Taylor Parrish <twpearish@gmail.com>
2018-08-28 03:00:23 -0700

    fiddling with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// console.log(path.join(__dirname, 'public'));
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV == 'development')
-    require('dotenv').config({ path: '.env.development' });
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -47,16 +41,13 @@ module.exports = (env) => {
             }]
         },
         plugins: [
+            new webpack.DefinePlugin({           
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),      
+                INSTA_TOKEN: JSON.stringify(process.env.INSTA_TOKEN),
+                INSTA_CLIENT_ID: JSON.stringify(process.env.INSTA_CLIENT_ID),
+              }),
             CSSExtract,
-            new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-            })
-        ],
+        ], 
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
